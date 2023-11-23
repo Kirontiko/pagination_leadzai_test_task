a1794c368fff40129ade0cfbfbd9efa4d5dd39d7
Denys Seliverstov <catsperr322@gmail.com>
2023-11-23 13:18:07 +0200

    Fixing some bugs with edge cases, adding more test to cover more cases

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+def add_pages(pages: list, start: int, end: int) -> list:
+    pages.extend(range(start, end + 1))
+    return pages
+
+
 def paginate(current_page: int,
              total_pages: int,
              boundaries: int,
@@ -9,8 +14,8 @@ def paginate(current_page: int,
     if not all(arg >= 0 for arg in args):
         raise ValueError("All arguments must be positive integers")
 
-    if not boundaries > 0:
-        raise ValueError("You can`t make pagination without at least boundaries of 1")
+    if boundaries < 0:
+        raise ValueError("Boundaries can`t be less than 0")
 
     if not total_pages > 0:
         raise ValueError("You can`t make pagination with 0 pages")
@@ -19,28 +24,49 @@ def paginate(current_page: int,
         raise ValueError("Your current page is not in range of available pages")
 
     pages_list = []
+    if boundaries >= total_pages // 2:
+        pages_list = list([str(page) for page in range(1, total_pages + 1)])
+        pages_list = " ".join(pages_list)
+        print(pages_list)
+        return pages_list
+    if boundaries == 0:
+        pages_list.insert(0, 1)
 
-    def add_pages(start: int, end: int):
-        pages_list.extend(range(start, end + 1))
-
-    add_pages(1, min(boundaries, total_pages))
-
-    before_current_page = max(1, current_page - around)
-    if before_current_page > boundaries + 1:
+    add_pages(pages_list, 1, min(boundaries, total_pages))
+    if current_page >= total_pages - boundaries:
+        if boundaries < around:
+            before_current_page = max(1, current_page - around)
+        elif boundaries == around:
+            before_current_page = max(1, current_page - boundaries)
+        else:
+            before_current_page = max(1, current_page - boundaries + 1)
+    else:
+        before_current_page = max(1, current_page - around)
+    if before_current_page > boundaries + 2:
         pages_list.append('...')
     after_current_page = min(total_pages, current_page + around)
 
-    add_pages(max(boundaries + 1, before_current_page), after_current_page)
+    add_pages(pages_list, max(boundaries + 1, before_current_page), after_current_page)
 
-    if after_current_page < total_pages - boundaries:
+    if after_current_page < total_pages - boundaries - 1:
         pages_list.append('...')
 
-    add_pages(max(total_pages - boundaries + 1, after_current_page + 1), total_pages)
-    pages_list = [str(el) for el in pages_list]
+    add_pages(pages_list, max(total_pages - boundaries + 1, after_current_page + 1), total_pages)
+    if boundaries == 0 and total_pages not in pages_list:
+        pages_list.append(total_pages)
+
+    pages_list = [str(page) for page in pages_list]
     pages_list = " ".join(pages_list)
+
     print(pages_list)
     return pages_list
 
 
 if __name__ == "__main__":
-    paginate(3, 10, 2, 2)
+    paginate(10, 10, 3, 1)
+    paginate(5, 10, 11, 1)
+    paginate(1, 10, 6, 1)
+    paginate(3, 10, 0, 0)
+    paginate(4, 5, 1, 0)
+    paginate(6, 11, 1, 2)
+    paginate(10, 10, 2, 2)
--- test_main.py
+++ test_main.py
@@ -9,6 +9,10 @@ class TestPagination(unittest.TestCase):
         result = paginate(4, 10, 2, 2)
         self.assertEqual(result, "1 2 3 4 5 6 ... 9 10")
 
+    def test_pagination_with_around_equals_zero(self):
+        result = paginate(4, 5, 1, 0)
+        self.assertEqual(result, "1 ... 4 5")
+
     def test_curent_page_in_the_middle_of_all_pages(self):
         result = paginate(50, 100, 2, 2)
         self.assertEqual(result, "1 2 ... 48 49 50 51 52 ... 99 100")
@@ -17,38 +21,74 @@ class TestPagination(unittest.TestCase):
         with self.assertRaises(ValueError):
             paginate(1, 0, 2, 2)
 
-    def test_negative_values(self):
-        values_list = [
-            [-1, 10, 2, 2],
-            [1, -10, 2, 2],
-            [1, 10, -2, 2],
-            [1, 10, 2, -2]
-        ]
+    def test_negative_current_pages_value(self):
         with self.assertRaises(ValueError):
-            for values in values_list:
-                paginate(*values)
+            paginate(-1, 10, 2, 2)
 
-    def test_current_page_out_of_range(self):
+    def test_negative_total_pages_value(self):
+        with self.assertRaises(ValueError):
+            paginate(1, -1, 2, 2)
+
+    def test_negative_boundaries_value(self):
+        with self.assertRaises(ValueError):
+            paginate(1, 1, -1, 2)
+
+    def test_negative_around_value(self):
+        with self.assertRaises(ValueError):
+            paginate(1, 1, 1, -1)
+
+    def test_current_page_is_greater_than_total_pages(self):
         with self.assertRaises(ValueError):
             paginate(15, 10, 2, 2)
 
+    def test_current_page_is_less_than_total_pages(self):
         with self.assertRaises(ValueError):
             paginate(0, 10, 2, 2)
 
-    def test_invalid_argument_types(self):
+    def test_invalid_type_of_current_page_argument(self):
         with self.assertRaises(TypeError):
-            paginate('1', 10, 2, 2)
-
+            paginate("1", 10, 2, 2)
+    def test_invalid_type_of_total_pages_argument(self):
         with self.assertRaises(TypeError):
-            paginate(1, 10, 2, '2')
+            paginate(1, "10", 2, 2)
+    def test_invalid_type_of_boundaries_argument(self):
+        with self.assertRaises(TypeError):
+            paginate(1, 10, "2", 2)
+    def test_invalid_type_of_around_argument(self):
+        with self.assertRaises(TypeError):
+            paginate(1, 10, 2, "2")
 
     def test_pagination_with_one_page(self):
         result = paginate(1, 1, 1, 0)
         self.assertEqual(result, "1")
 
     def test_pagination_with_no_boundaries(self):
-        with self.assertRaises(ValueError):
-            paginate(1, 1, 0, 1)
+        result = paginate(3, 10, 0, 0)
+        self.assertEqual(result, "1 ... 3 ... 10")
+
+    def test_pagination_with_boundaries_is_greater_than_total_pages(self):
+        result = paginate(3, 10, 10000, 0)
+        self.assertEqual(result, "1 2 3 4 5 6 7 8 9 10")
+
+    def test_pagination_with_boundaries_equal_total_pages(self):
+        result = paginate(5, 5, 5, 0)
+        self.assertEqual(result, "1 2 3 4 5")
+
+    def test_pagination_with_around_completely_inside_left_boundary(self):
+        result = paginate(10, 10, 3, 1)
+        self.assertEqual(result, "1 2 3 ... 8 9 10")
+
+    def test_pagination_with_around_completely_inside_right_boundary(self):
+        result = paginate(2, 10, 3, 1)
+        self.assertEqual(result, "1 2 3 ... 8 9 10")
+
+    def test_pagination_with_current_page_on_last_page(self):
+        result = paginate(10, 10, 2, 2)
+        self.assertEqual(result, "1 2 ... 8 9 10")
+
+    def test_pagination_with_current_page_on_first_page(self):
+        result = paginate(1, 10, 2, 1)
+        self.assertEqual(result, "1 2 ... 9 10")
 
 
 if __name__ == "__main__":
